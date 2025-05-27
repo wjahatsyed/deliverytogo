58c836d11b8364f2aaf89c23d8e1770222387555
Wajahat Adil <wajahatsyed1257@gmail.com>
2025-05-27 23:34:44 +0500

    feat(discovery-server):add eureka server for service discovery

---
+++ discovery-server/src/test/java/com/deliverytogo/discovery_server/DiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deliverytogo.discovery_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
